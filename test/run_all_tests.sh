8c0ee5b383f259ee68109818db54d35cacc082ab
jakvitov <jakvitov@gmail.com>
2023-12-28 13:42:59 +0100

    Testing shell script

---
+++ test/run_all_tests.sh
@@ -0,0 +1,17 @@
+#Runs all tests in the names packages named in ./testpackages
+file="./test_packages"
+
+# Check if the file exists
+if [ ! -f "$file" ]; then
+    echo "File not found: $file"
+    exit 1
+fi
+
+mapfile -t dirs < "$file"
+
+for dir in "${dirs[@]}"; do
+    echo "Testing in: $dir" 
+    go test $dir
+done
+
+echo "Tests completed"
